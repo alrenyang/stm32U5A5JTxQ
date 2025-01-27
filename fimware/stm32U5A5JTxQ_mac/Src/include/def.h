c333beb46d99d43b852574f8c15473c9313586e0
alrenyang <alren.yk@gmail.com>
2025-01-28 08:00:59 +0900

    change floder

--- fimware/stm32U5A5JTxQ_mac/Src/include/def.h
+++
@@ -1,9 +0,0 @@
-
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdarg.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define Firware_10_0_1
--- fimware/stm32u5a_cmake/smt32u5a5/Src/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#include "bsp.h"
-#include "hw.h"
-
-#endif

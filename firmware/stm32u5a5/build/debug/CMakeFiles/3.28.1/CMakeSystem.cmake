60dc939e1b8533404788a71af0206b3b70386043
alrenyang <alren.yk@gmail.com>
2025-01-28 11:53:40 +0900

    device_edit

--- firmware/stm32u5a5/build/debug/CMakeFiles/3.28.1/CMakeSystem.cmake
+++ firmware/stm32u5a5/build/debug/CMakeFiles/3.28.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "24.2.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
 
-include("/Users/alrenm3/program/stm32u5a5/stm32U5A5JTxQ/firmware/stm32u5a5/cmake/gcc-arm-none-eabi.cmake")
+include("/Users/alrenm3/program/stm32u5a5/firmware/stm32u5a5/cmake/gcc-arm-none-eabi.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
--- firmware/stm32u5a5/build/debug/cmake_install.cmake
+++ firmware/stm32u5a5/build/debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/alrenm3/program/stm32u5a5/stm32U5A5JTxQ/firmware/stm32u5a5
+# Install script for directory: /Users/alrenm3/program/stm32u5a5/firmware/stm32u5a5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/alrenm3/program/stm32u5a5/stm32U5A5JTxQ/firmware/stm32u5a5/build/debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/alrenm3/program/stm32u5a5/firmware/stm32u5a5/build/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
